9688fb69872299bddacf2857de242caf177d5244
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-04-29 02:45:29 +0300

    reduxToolkit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,17 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
-const Cart = (props) => {
+const Cart = props => {
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
         <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
+          item={{ title: "Test Item", quantity: 3, total: 18, price: 6 }}
+        />
+        <CartItem
+          item={{ title: "Test Item", quantity: 3, total: 18, price: 6 }}
         />
       </ul>
     </Card>
